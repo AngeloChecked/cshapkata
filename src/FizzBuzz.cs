50b7bb13635a1c2b04238800d18af987da8c9c04
MauroMonteneri <107854959+MauroMonteneri@users.noreply.github.com>
2022-11-18 15:41:44 +0100

    concatenated result

--- src/FizzBuzz.cs
+++ src/FizzBuzz.cs
@@ -5,21 +5,16 @@ public class FizzBuzz : IFizzBuzz
     public string Say(int number)
     {
         
-        if (number % 3 == 0 && number % 5 == 0)
-        {
-            return "FizzBuzz";
-        }
-
         string result = String.Empty;
 
         if (number % 3 == 0)
         {
-            result = "Fizz";
+            result += "Fizz";
         }
 
         if (number % 5 == 0)
         {
-            result = "Buzz";
+            result += "Buzz";
         }
 
         return String.IsNullOrEmpty(result) ? number.ToString() : result;
